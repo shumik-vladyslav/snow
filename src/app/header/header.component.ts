02d9683413002c509ca989157376df30d6b643f3
organatik <organatik@gmail.com>
2017-02-21 13:30:34 +0200

    scroll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {DataService} from "../shared/service/data.service";
-
+declare var $:any;
 
 @Component({
   selector: 'header',
@@ -13,6 +13,13 @@ export class HeaderComponent {
   constructor(private dataService: DataService){
     dataService.headerText.subscribe((data) => {
       this.text = data;
-    })
+    });
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 20) {
+        $('#task_flyout').addClass('fixed');
+      } else {
+        $('#task_flyout').removeClass('fixed');
+      }
+    });
   }
 }
